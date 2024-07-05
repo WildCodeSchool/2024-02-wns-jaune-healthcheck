53c6da820225f790313610de6096aaba0f8dd344
Skullyfox.yt@gmail.com <Skullyfox.yt@gmail.com>
2024-07-05 14:41:05 +0200

    WILD-007 : Refacto - Liaison page URL au backend

--- front/src/generated/graphql-types.ts
+++ front/src/generated/graphql-types.ts
@@ -1,108 +1,158 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type Exact<T extends { [key: string]: unknown }> = {
+    [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+    [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+    [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+    T extends { [key: string]: unknown },
+    K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+    | T
+    | {
+          [P in keyof T]?: P extends " $fragmentName" | "__typename"
+              ? T[P]
+              : never;
+      };
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-  DateTimeISO: { input: any; output: any; }
+    ID: { input: string; output: string };
+    String: { input: string; output: string };
+    Boolean: { input: boolean; output: boolean };
+    Int: { input: number; output: number };
+    Float: { input: number; output: number };
+    DateTimeISO: { input: any; output: any };
 };
 
 export type History = {
-  __typename?: 'History';
-  created_at: Scalars['DateTimeISO']['output'];
-  id: Scalars['String']['output'];
-  response: Scalars['String']['output'];
-  status_code: Scalars['Float']['output'];
-  url: Url;
+    __typename?: "History";
+    created_at: Scalars["DateTimeISO"]["output"];
+    id: Scalars["String"]["output"];
+    response: Scalars["String"]["output"];
+    status_code: Scalars["Float"]["output"];
+    url: Url;
 };
 
 export type HistoryInput = {
-  response: Scalars['String']['input'];
-  status_code: Scalars['Float']['input'];
-  urlId: Scalars['String']['input'];
+    response: Scalars["String"]["input"];
+    status_code: Scalars["Float"]["input"];
+    urlId: Scalars["String"]["input"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  addHistory: History;
-  addUrl: Url;
+    __typename?: "Mutation";
+    addHistory: History;
+    addUrl: Url;
 };
-
 
 export type MutationAddHistoryArgs = {
-  historyData: HistoryInput;
+    historyData: HistoryInput;
 };
 
-
 export type MutationAddUrlArgs = {
-  urlData: UrlInput;
+    urlData: UrlInput;
 };
 
 export type Query = {
-  __typename?: 'Query';
-  histories: Array<History>;
-  history: History;
-  url: Url;
-  urls: Array<Url>;
+    __typename?: "Query";
+    histories: Array<History>;
+    history: History;
+    url: Url;
+    urls: Array<Url>;
 };
-
 
 export type QueryHistoryArgs = {
-  id: Scalars['String']['input'];
+    id: Scalars["String"]["input"];
 };
 
-
 export type QueryUrlArgs = {
-  id: Scalars['String']['input'];
+    id: Scalars["String"]["input"];
 };
 
 export type Url = {
-  __typename?: 'Url';
-  createdAt: Scalars['DateTimeISO']['output'];
-  histories: Array<History>;
-  id: Scalars['String']['output'];
-  name: Scalars['String']['output'];
-  path: Scalars['String']['output'];
+    __typename?: "Url";
+    createdAt: Scalars["DateTimeISO"]["output"];
+    histories: Array<History>;
+    id: Scalars["String"]["output"];
+    name: Scalars["String"]["output"];
+    path: Scalars["String"]["output"];
 };
 
 export type UrlInput = {
-  name: Scalars['String']['input'];
-  path: Scalars['String']['input'];
+    name: Scalars["String"]["input"];
+    path: Scalars["String"]["input"];
 };
 
 export type AddUrlMutationVariables = Exact<{
-  urlData: UrlInput;
+    urlData: UrlInput;
 }>;
 
+export type AddUrlMutation = {
+    __typename?: "Mutation";
+    addUrl: { __typename?: "Url"; name: string; path: string };
+};
 
-export type AddUrlMutation = { __typename?: 'Mutation', addUrl: { __typename?: 'Url', name: string, path: string } };
+export type GetAllURlsQueryVariables = Exact<{ [key: string]: never }>;
 
-export type GetAllURlsQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetAllURlsQuery = {
+    __typename?: "Query";
+    urls: Array<{
+        __typename?: "Url";
+        id: string;
+        name: string;
+        path: string;
+        createdAt: any;
+        histories: Array<{
+            __typename?: "History";
+            id: string;
+            response: string;
+            status_code: number;
+        }>;
+    }>;
+};
 
+export type UrlQueryVariables = Exact<{
+    urlId: Scalars["String"]["input"];
+}>;
 
-export type GetAllURlsQuery = { __typename?: 'Query', urls: Array<{ __typename?: 'Url', id: string, name: string, path: string, createdAt: any, histories: Array<{ __typename?: 'History', id: string, response: string, status_code: number }> }> };
-
+export type UrlQuery = {
+    __typename?: "Query";
+    url: {
+        __typename?: "Url";
+        id: string;
+        name: string;
+        path: string;
+        histories: Array<{
+            __typename?: "History";
+            id: string;
+            response: string;
+            status_code: number;
+            created_at: any;
+        }>;
+    };
+};
 
 export const AddUrlDocument = gql`
     mutation AddUrl($urlData: UrlInput!) {
-  addUrl(urlData: $urlData) {
-    name
-    path
-  }
-}
-    `;
-export type AddUrlMutationFn = Apollo.MutationFunction<AddUrlMutation, AddUrlMutationVariables>;
+        addUrl(urlData: $urlData) {
+            name
+            path
+        }
+    }
+`;
+export type AddUrlMutationFn = Apollo.MutationFunction<
+    AddUrlMutation,
+    AddUrlMutationVariables
+>;
 
 /**
  * __useAddUrlMutation__
@@ -121,28 +171,39 @@ export type AddUrlMutationFn = Apollo.MutationFunction<AddUrlMutation, AddUrlMut
  *   },
  * });
  */
-export function useAddUrlMutation(baseOptions?: Apollo.MutationHookOptions<AddUrlMutation, AddUrlMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<AddUrlMutation, AddUrlMutationVariables>(AddUrlDocument, options);
-      }
+export function useAddUrlMutation(
+    baseOptions?: Apollo.MutationHookOptions<
+        AddUrlMutation,
+        AddUrlMutationVariables
+    >,
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useMutation<AddUrlMutation, AddUrlMutationVariables>(
+        AddUrlDocument,
+        options,
+    );
+}
 export type AddUrlMutationHookResult = ReturnType<typeof useAddUrlMutation>;
 export type AddUrlMutationResult = Apollo.MutationResult<AddUrlMutation>;
-export type AddUrlMutationOptions = Apollo.BaseMutationOptions<AddUrlMutation, AddUrlMutationVariables>;
+export type AddUrlMutationOptions = Apollo.BaseMutationOptions<
+    AddUrlMutation,
+    AddUrlMutationVariables
+>;
 export const GetAllURlsDocument = gql`
     query GetAllURls {
-  urls {
-    id
-    name
-    path
-    createdAt
-    histories {
-      id
-      response
-      status_code
+        urls {
+            id
+            name
+            path
+            createdAt
+            histories {
+                id
+                response
+                status_code
+            }
+        }
     }
-  }
-}
-    `;
+`;
 
 /**
  * __useGetAllURlsQuery__
@@ -159,19 +220,111 @@ export const GetAllURlsDocument = gql`
  *   },
  * });
  */
-export function useGetAllURlsQuery(baseOptions?: Apollo.QueryHookOptions<GetAllURlsQuery, GetAllURlsQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetAllURlsQuery, GetAllURlsQueryVariables>(GetAllURlsDocument, options);
-      }
-export function useGetAllURlsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAllURlsQuery, GetAllURlsQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetAllURlsQuery, GetAllURlsQueryVariables>(GetAllURlsDocument, options);
-        }
-export function useGetAllURlsSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetAllURlsQuery, GetAllURlsQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useSuspenseQuery<GetAllURlsQuery, GetAllURlsQueryVariables>(GetAllURlsDocument, options);
-        }
+export function useGetAllURlsQuery(
+    baseOptions?: Apollo.QueryHookOptions<
+        GetAllURlsQuery,
+        GetAllURlsQueryVariables
+    >,
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useQuery<GetAllURlsQuery, GetAllURlsQueryVariables>(
+        GetAllURlsDocument,
+        options,
+    );
+}
+export function useGetAllURlsLazyQuery(
+    baseOptions?: Apollo.LazyQueryHookOptions<
+        GetAllURlsQuery,
+        GetAllURlsQueryVariables
+    >,
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useLazyQuery<GetAllURlsQuery, GetAllURlsQueryVariables>(
+        GetAllURlsDocument,
+        options,
+    );
+}
+export function useGetAllURlsSuspenseQuery(
+    baseOptions?: Apollo.SuspenseQueryHookOptions<
+        GetAllURlsQuery,
+        GetAllURlsQueryVariables
+    >,
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useSuspenseQuery<GetAllURlsQuery, GetAllURlsQueryVariables>(
+        GetAllURlsDocument,
+        options,
+    );
+}
 export type GetAllURlsQueryHookResult = ReturnType<typeof useGetAllURlsQuery>;
-export type GetAllURlsLazyQueryHookResult = ReturnType<typeof useGetAllURlsLazyQuery>;
-export type GetAllURlsSuspenseQueryHookResult = ReturnType<typeof useGetAllURlsSuspenseQuery>;
-export type GetAllURlsQueryResult = Apollo.QueryResult<GetAllURlsQuery, GetAllURlsQueryVariables>;
+export type GetAllURlsLazyQueryHookResult = ReturnType<
+    typeof useGetAllURlsLazyQuery
+>;
+export type GetAllURlsSuspenseQueryHookResult = ReturnType<
+    typeof useGetAllURlsSuspenseQuery
+>;
+export type GetAllURlsQueryResult = Apollo.QueryResult<
+    GetAllURlsQuery,
+    GetAllURlsQueryVariables
+>;
+export const UrlDocument = gql`
+    query Url($urlId: String!) {
+        url(id: $urlId) {
+            histories {
+                id
+                response
+                status_code
+                created_at
+            }
+            id
+            name
+            path
+        }
+    }
+`;
+
+/**
+ * __useUrlQuery__
+ *
+ * To run a query within a React component, call `useUrlQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUrlQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUrlQuery({
+ *   variables: {
+ *      urlId: // value for 'urlId'
+ *   },
+ * });
+ */
+export function useUrlQuery(
+    baseOptions: Apollo.QueryHookOptions<UrlQuery, UrlQueryVariables> &
+        ({ variables: UrlQueryVariables; skip?: boolean } | { skip: boolean }),
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useQuery<UrlQuery, UrlQueryVariables>(UrlDocument, options);
+}
+export function useUrlLazyQuery(
+    baseOptions?: Apollo.LazyQueryHookOptions<UrlQuery, UrlQueryVariables>,
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useLazyQuery<UrlQuery, UrlQueryVariables>(
+        UrlDocument,
+        options,
+    );
+}
+export function useUrlSuspenseQuery(
+    baseOptions?: Apollo.SuspenseQueryHookOptions<UrlQuery, UrlQueryVariables>,
+) {
+    const options = { ...defaultOptions, ...baseOptions };
+    return Apollo.useSuspenseQuery<UrlQuery, UrlQueryVariables>(
+        UrlDocument,
+        options,
+    );
+}
+export type UrlQueryHookResult = ReturnType<typeof useUrlQuery>;
+export type UrlLazyQueryHookResult = ReturnType<typeof useUrlLazyQuery>;
+export type UrlSuspenseQueryHookResult = ReturnType<typeof useUrlSuspenseQuery>;
+export type UrlQueryResult = Apollo.QueryResult<UrlQuery, UrlQueryVariables>;
