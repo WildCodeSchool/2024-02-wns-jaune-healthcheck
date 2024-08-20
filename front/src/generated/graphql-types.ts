643282d40c1e4a336ddba9d31b57024f957dbee6
Jerome Tissot <jerome.tissot@lamache.org>
2024-08-20 11:57:51 +0200

    Commit before pull develop

--- front/src/generated/graphql-types.ts
+++ front/src/generated/graphql-types.ts
@@ -30,13 +30,19 @@ export type History = {
 export type Mutation = {
   __typename?: 'Mutation';
   addUrl: Url;
+  addUserUrl: Url;
   createUser: Scalars['String']['output'];
   login: Scalars['String']['output'];
 };
 
 
-
 export type MutationAddUrlArgs = {
+  urlData: UrlInput;
+};
+
+
+export type MutationAddUserUrlArgs = {
+  isPrivate: Scalars['Boolean']['input'];
   urlData: UrlInput;
 };
 
@@ -68,10 +74,10 @@ export type Query = {
   history: History;
   logout: Scalars['String']['output'];
   me: Scalars['String']['output'];
+  recentPrivateUrls: Array<Url>;
   url: Url;
-  urls: Array<Url>;
+  urls: PaginateUrls;
 };
-
 
 
 export type QueryHistoryArgs = {
@@ -79,11 +85,9 @@ export type QueryHistoryArgs = {
 };
 
 
-
 export type QueryUrlArgs = {
   id: Scalars['String']['input'];
 };
-
 
 
 export type QueryUrlsArgs = {
@@ -99,6 +103,7 @@ export type Url = {
   id: Scalars['String']['output'];
   name: Scalars['String']['output'];
   path: Scalars['String']['output'];
+  userUrl?: Maybe<UserUrl>;
 };
 
 export type UrlInput = {
@@ -112,8 +117,6 @@ export type UserUrl = {
   id: Scalars['String']['output'];
   urlId: Scalars['String']['output'];
   userId: Scalars['String']['output'];
-  name: Scalars['String']['input'];
-  path: Scalars['String']['input'];
 };
 
 export type AddUrlMutationVariables = Exact<{
@@ -138,15 +141,21 @@ export type LoginMutationVariables = Exact<{
 }>;
 
 
-export type LoginMutation = { __typename?: "Mutation"; login: string };
-export type LoginMutation = { __typename?: "Mutation"; login: string };
+export type LoginMutation = { __typename?: 'Mutation', login: string };
+
+export type AddUserUrlMutationVariables = Exact<{
+  urlData: UrlInput;
+  isPrivate: Scalars['Boolean']['input'];
+}>;
+
+
+export type AddUserUrlMutation = { __typename?: 'Mutation', addUserUrl: { __typename?: 'Url', name: string, path: string } };
 
 export type GetAllURlsQueryVariables = Exact<{
   currentPage: Scalars['Float']['input'];
   sortField: Scalars['String']['input'];
   searchText: Scalars['String']['input'];
 }>;
-
 
 
 export type GetAllURlsQuery = { __typename?: 'Query', urls: { __typename?: 'PaginateUrls', totalPages: number, currentPage: number, previousPage: number, nextPage: number, urls: Array<{ __typename?: 'Url', id: string, name: string, path: string, createdAt: any, histories: Array<{ __typename?: 'History', id: string, response: string, status_code: number, created_at: any }> }> } };
@@ -163,14 +172,12 @@ export type RecentPrivateUrlsQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type RecentPrivateUrlsQuery = { __typename?: 'Query', recentPrivateUrls: Array<{ __typename?: 'Url', id: string, name: string, path: string, createdAt: any, histories: Array<{ __typename?: 'History', id: string, status_code: number, created_at: any }> }> };
 
-export type LogoutQueryVariables = Exact<{ [key: string]: never;; }>;
-
+export type LogoutQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type LogoutQuery = { __typename?: 'Query', logout: string };
 
-export type MeQueryVariables = Exact<{ [key: string]: never;; }>;
-
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type MeQuery = { __typename?: 'Query', me: string };
@@ -275,21 +282,63 @@ export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginM
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
 export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export const AddUserUrlDocument = gql`
+    mutation AddUserUrl($urlData: UrlInput!, $isPrivate: Boolean!) {
+  addUserUrl(urlData: $urlData, isPrivate: $isPrivate) {
+    name
+    path
+  }
+}
+    `;
+export type AddUserUrlMutationFn = Apollo.MutationFunction<AddUserUrlMutation, AddUserUrlMutationVariables>;
+
+/**
+ * __useAddUserUrlMutation__
+ *
+ * To run a mutation, you first call `useAddUserUrlMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useAddUserUrlMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [addUserUrlMutation, { data, loading, error }] = useAddUserUrlMutation({
+ *   variables: {
+ *      urlData: // value for 'urlData'
+ *      isPrivate: // value for 'isPrivate'
+ *   },
+ * });
+ */
+export function useAddUserUrlMutation(baseOptions?: Apollo.MutationHookOptions<AddUserUrlMutation, AddUserUrlMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<AddUserUrlMutation, AddUserUrlMutationVariables>(AddUserUrlDocument, options);
+      }
+export type AddUserUrlMutationHookResult = ReturnType<typeof useAddUserUrlMutation>;
+export type AddUserUrlMutationResult = Apollo.MutationResult<AddUserUrlMutation>;
+export type AddUserUrlMutationOptions = Apollo.BaseMutationOptions<AddUserUrlMutation, AddUserUrlMutationVariables>;
 export const GetAllURlsDocument = gql`
-    query GetAllURls($sortField: String!, $searchText: String!) {
-        urls(sortField: $sortField, searchText: $searchText) {
-            id
-            name
-            path
-            createdAt
-            histories {
-                id
-                response
-                status_code
-            }
-        }
+    query GetAllURls($currentPage: Float!, $sortField: String!, $searchText: String!) {
+  urls(currentPage: $currentPage, sortField: $sortField, searchText: $searchText) {
+    urls {
+      id
+      name
+      path
+      createdAt
+      histories {
+        id
+        response
+        status_code
+        created_at
+      }
     }
-`;
+    totalPages
+    currentPage
+    previousPage
+    nextPage
+  }
+}
+    `;
 
 /**
  * __useGetAllURlsQuery__
