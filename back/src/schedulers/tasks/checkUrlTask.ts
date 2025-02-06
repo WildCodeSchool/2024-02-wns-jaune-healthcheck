9b775c1c871f2796f60ea28c992991c3da389abf
Jerome Tissot <jerome.tissot@lamache.org>
2025-02-06 12:24:45 +0100

    Debug v5

--- back/src/schedulers/tasks/checkUrlTask.ts
+++ back/src/schedulers/tasks/checkUrlTask.ts
@@ -27,10 +27,9 @@ const createOrUpdateNotification = async (newHistory: History) => {
 
     if (user && user.notifications) {
         for (const notification of user.notifications) {
-            console.log('notification ==> ', notification);
-            if (
-                notification.history.url.path === newHistory.url.path &&
-                notification.history.status_code === newHistory.status_code
+            if ( 
+                notification.history?.url.path === newHistory.url.path &&
+                notification.history?.status_code === newHistory.status_code
             ) {
                 notification.history.notification = null;
 
